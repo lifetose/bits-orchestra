dd4ef05fd74f4ecdb349bcecacaf7fdb882c746b
sashalehedza <lehedzagot@gmail.com>
2025-03-11 11:04:14 +0200

    fix

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,5 +1,9 @@
 function NotFound() {
-  return <div>Page Not Found</div>;
+  return (
+    <div className='flex justify-center items-center w-full'>
+      <h3 className='text-2xl font-bold text-red-600'>Page Not Found</h3>
+    </div>
+  );
 }
 
 export default NotFound;
